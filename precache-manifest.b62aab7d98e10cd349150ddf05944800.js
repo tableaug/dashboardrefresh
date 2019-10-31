90644b62aac7628795ecd806f4e99c23444f12ac
tableaug <31282196+tableaug@users.noreply.github.com>
2019-10-31 10:46:07 +0000

    first commit

---
+++ precache-manifest.b62aab7d98e10cd349150ddf05944800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccb08e0a80f8603124116061a5127a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d0ad1b5079535286d36",
+    "url": "/static/css/main.f93d3918.chunk.css"
+  },
+  {
+    "revision": "51fff23d8fcfc482d5ad",
+    "url": "/static/js/2.f83430b2.chunk.js"
+  },
+  {
+    "revision": "2d0ad1b5079535286d36",
+    "url": "/static/js/main.3e0611b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c8d5ff028065c338755d808643e40ca5",
+    "url": "/static/media/logo.c8d5ff02.svg"
+  }
+]);
